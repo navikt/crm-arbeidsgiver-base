51522c162cee16082585ca9c3472136e53103778
suddal <stine@uddal.com>
2020-04-14 22:59:18 +0200

    Changed logic in lwc

--- force-app/main/accountMessages/lwc/accountMessages/accountMessages.js
+++ force-app/main/accountMessages/lwc/accountMessages/accountMessages.js
@@ -1,43 +1,37 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import getAccountHistory from '@salesforce/apex/AccountMessagesController.getAccountHistory';
-import getParentAccountHistory from '@salesforce/apex/AccountMessagesController.getParentAccountHistory';
+import getData from '@salesforce/apex/AccountMessagesController.getData';
 import { getRecord } from 'lightning/uiRecordApi';
 
 
 export default class AccountMessages extends LightningElement {
 	@api recordId;
+	@track isBankrupt;
+	@track isInLiquidation;
+	@track isInForcedSettlementOrResolution;
+
 	accountHistory;
 	parentAccountHistory;
 
 
 	connectedCallback() {
-		this.loadAccountHistory();
-		this.loadParentAccountHistory();
-
+		this.loadData();
 	}
-
-	loadAccountHistory() {
-		getAccountHistory({ recordId: this.recordId })
+	loadData() {
+		getData({ recordId: this.recordId })
 			.then(result => {
 				this.accountHistory = result;
+				if (result) {
+					this.isBankrupt = result.isBankrupt;
+					this.isInLiquidation = result.isInLiquidation;
+					this.isInForcedSettlementOrResolution = result.isInForcedSettlementOrResolution;
+				}
 				this.error = undefined;
-			})
-			.catch(error => {
-				this.error = error;
-				this.accountHistory = undefined;
-			});
-	}
 
-	loadParentAccountHistory() {
-		getParentAccountHistory({ recordId: this.recordId })
-			.then(result => {
-				this.parentAccountHistory = result;
-				this.error = undefined;
 			})
 			.catch(error => {
 				this.error = error;
 				this.parentAccountHistory = undefined;
-			});
-	}
 
+			})
+	}
 }
