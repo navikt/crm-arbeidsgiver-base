b51086e32b9061105f7f54310acc22623a658905
johnforeland <john.foreland@me.com>
2020-04-14 14:04:21 +0200

    Initial view of arbeidsplassen viewer

--- force-app/main/arbeidsplassen/lwc/arbeidsplassenView/arbeidsplassenView.js
+++ force-app/main/arbeidsplassen/lwc/arbeidsplassenView/arbeidsplassenView.js
@@ -1,3 +1,51 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
+import getdata from "@salesforce/apex/ArbeidsplassenController.getData";
+import * as helper from "./helper";
+import columns from "./columns";
 
-export default class ArbeidsplassenView extends LightningElement {}
+
+export default class ArbeidsplassenView extends LightningElement {
+
+	@api recordId;
+
+	// fetch data
+	wiredData;
+	@track data;
+	@track columns = columns.columns;
+
+	// for sorting
+	@track sortBy;
+	@track sortDirection;
+
+
+	@wire(getdata, { recordId: '$recordId' })
+	deWire(result) {
+		this.wiredData = result;
+		if (result.data) {
+			this.data = result.data;
+		} else if (result.error) {
+			// this.setError(result.error);
+		}
+	}
+
+	updateColumnSorting(event) {
+		this.sortBy = event.detail.fieldName;
+		this.sortDirection = event.detail.sortDirection;
+		this.data = helper.sortData(this.data, this.sortBy, this.sortDirection);
+	}
+
+	handleRowAction(event) {
+		const action = event.detail.action;
+		const row = event.detail.row;
+
+		switch (action.name) {
+
+			case "openUrl":
+				window.open(row.link, "_blank");
+				break;
+			default:
+				break;
+		}
+	}
+
+}
