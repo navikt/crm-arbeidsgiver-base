ccff2fb8744d50459ee0ba2d1369df40bd3dbb99
suddal <stine@uddal.com>
2020-04-20 22:47:55 +0200

    Updated tests

--- force-app/main/accountMessages/lwc/accountMessages/accountMessages.js
+++ force-app/main/accountMessages/lwc/accountMessages/accountMessages.js
@@ -7,6 +7,12 @@ export default class AccountMessages extends LightningElement {
 	@api recordId;
 	@track accountHistory;
 	parentAccountHistory;
+	isBankrupt = false;
+	isNotBankrupt = false;
+	isInLiquidation = false;
+	isInForcedSettlementOrResolution = false;
+	list = [];
+
 
 	connectedCallback() {
 		this.loadData();
@@ -17,6 +23,27 @@ export default class AccountMessages extends LightningElement {
 				this.accountHistory = result;
 				this.error = undefined;
 
+				console.log('result', result);
+				for (let i = 0; i < result.length; i++) {
+
+					let field = result[i].field;
+					let isNew = result[i].isNew;
+
+					if (field === 'isBankrupt' && isNew === true) {
+						this.isBankrupt = true;
+						this.eventDate = result[i].eventDate;
+					} else if (field === 'isInLiquidation' && isNew === true) {
+						this.isInLiquidation = true;
+						this.eventDate = result[i].eventDate;
+					} else if (field === 'isInForcedSettlementOrResolution' && isNew === true) {
+						this.isInForcedSettlementOrResolution = true;
+						this.eventDate = result[i].eventDate;
+					}
+					//this.eventDate = result[i].eventDate;
+					/*this.isBankrupt = (field === 'isBankrupt');
+					this.isInLiquidation = (field === 'isInLiquidation');
+					this.isInForcedSettlementOrResolution = (field === 'isInForcedSettlementOrResolution');*/
+				}
 			})
 			.catch(error => {
 				this.error = error;
