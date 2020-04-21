edbbb3bc78bd65139ed3c226536264e5fe8384bd
suddal <stine@uddal.com>
2020-04-21 13:22:57 +0200

    Removed comments

--- force-app/main/accountMessages/lwc/accountMessages/accountMessages.js
+++ force-app/main/accountMessages/lwc/accountMessages/accountMessages.js
@@ -2,7 +2,6 @@ import { LightningElement, api, wire, track } from 'lwc';
 import getData from '@salesforce/apex/AccountMessagesController.getData';
 import { getRecord } from 'lightning/uiRecordApi';
 
-
 export default class AccountMessages extends LightningElement {
 	@api recordId;
 	@track accountHistory;
