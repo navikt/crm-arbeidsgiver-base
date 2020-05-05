3d8ade5787222c3c9eb1087933d1afddcc5ee716
suddal <stine@uddal.com>
2020-05-05 09:07:44 +0200

    Create test class for account team member controller

--- force-app/main/accountTeamMember/lwc/accountTeamMember/accountTeamMember.js
+++ force-app/main/accountTeamMember/lwc/accountTeamMember/accountTeamMember.js
@@ -5,7 +5,6 @@ import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 
 import getData from '@salesforce/apex/AccountTeamMemberController.getData';
 import deleteTeamMember from '@salesforce/apex/AccountTeamMemberController.deleteTeamMember';
-import addTeamMember from '@salesforce/apex/AccountTeamMemberController.addTeamMember';
 import { refreshApex } from '@salesforce/apex';
 
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
@@ -13,7 +12,6 @@ import Id from '@salesforce/user/Id';
 
 
 const actions = [
-	{ label: 'Edit', name: 'edit' },
 	{ label: 'Delete', name: 'delete' },
 ];
 
@@ -30,12 +28,10 @@ export default class AccountTeamMember extends NavigationMixin(LightningElement)
 	@api recordId;
 	@track data;
 	@track columns = columns;
-	@track showModal = false;
 
 	refreshTable;
 	error;
 	userId = Id;
-
 
 
 	@wire(getData, { recordId: '$recordId' })
@@ -58,18 +54,15 @@ export default class AccountTeamMember extends NavigationMixin(LightningElement)
 		}
 	}
 
-
-
 	handleRowActions(event) {
 		let actionName = event.detail.action.name;
 		let row = event.detail.row;
 
 		switch (actionName) {
-			case 'edit':
-				this.editCurrentRecord(row);
-				break;
 			case 'delete':
 				this.deleteRow(row);
+				break;
+			default:
 				break;
 		}
 	}
@@ -96,33 +89,25 @@ export default class AccountTeamMember extends NavigationMixin(LightningElement)
 			});
 	}
 
-	editCurrentRecord(currentRow) {
-		this[NavigationMixin.Navigate]({
-			type: 'standard__recordPage',
-			attributes: {
-				recordId: this.currentRow.Id,
-				objectApiName: 'AccountTeamMember', // objectApiName is optional
-				actionName: 'edit'
-			}
-		});
-	}
 	navigateToNewRecordPage() {
 		const defaultValues = encodeDefaultFieldValues({
 			AccountId: this.recordId,
 			UserId: this.userId
 		});
+
 		this[NavigationMixin.Navigate]({
 			type: 'standard__objectPage',
 			attributes: {
 				objectApiName: 'AccountTeamMember',
 				actionName: 'new'
+
 			}, state: {
-				nooverride: '1',
+				//nooverride: '1',
 				navigationLocation: 'LOOKUP',
 				defaultFieldValues: defaultValues
 			}
 		});
-		// refreshing table data using refresh apex
+
 		return refreshApex(this.refreshTable);
 	}
 }
