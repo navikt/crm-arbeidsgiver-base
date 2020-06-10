177586e067022dd545e5a6ba6266de1d77a6f005
suddal <stine@uddal.com>
2020-06-10 09:33:32 +0200

    Change component to alert

--- force-app/components/accountMessages/lwc/accountMessages/accountMessages.js
+++ force-app/components/accountMessages/lwc/accountMessages/accountMessages.js
@@ -1,22 +1,33 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import getData from '@salesforce/apex/AccountMessagesController.getData';
-import { getRecord } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation'
 
-export default class AccountMessages extends LightningElement {
-	@api recordId;
-	@track accountHistory;
-	@track noData = true;
+export default class AccountMessages extends NavigationMixin(LightningElement) {
+    @api recordId;
+    @track display = false;
+    @track accountId;
+    @track field;
 
-	connectedCallback() {
-		this.loadData();
-	}
-	loadData() {
-		getData({ recordId: this.recordId })
-			.then(result => {
-				this.accountHistory = result;
-				if (result.length > 0) {
-					this.noData = false;
-				}
-			});
-	}
+    connectedCallback() {
+        this.loadData();
+    }
+    loadData() {
+        getData({ recordId: this.recordId })
+            .then(result => {
+                if (result) {
+                    this.display = true;
+                    this.field = result.field;
+                    this.accountId = result.accountId;
+                }
+            });
+    }
+    openRecord() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.accountId,
+                actionName: 'view'
+            }
+        });
+    }
 }
