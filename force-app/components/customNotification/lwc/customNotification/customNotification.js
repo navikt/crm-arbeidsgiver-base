58a45c57f2b4ebd9070981c311279b2483354c52
John Arne Føreland <john.foreland@me.com>
2020-07-20 16:53:44 +0200

    Addec notification component to notify user of important information (#474)
    * Addec notification component to notify user of important information
    
    * sleep in github action before running tests
    
    * fixed tests

---
+++ force-app/components/customNotification/lwc/customNotification/customNotification.js
@@ -0,0 +1,37 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getFieldValue from '@salesforce/apex/CustomNotificationController.getFieldValue';
+
+export default class CustomNotification extends LightningElement {
+
+    @api title;
+    @api field;
+    @api variant;
+    @api icon;
+    @api recordId;
+
+    @track message;
+    @track mainCss = 'slds-notify slds-notify_alert slds-theme_alert-texture round ';
+    @track titleCss = 'slds-text-title_caps slds-var-p-right_large ';
+
+    connectedCallback() {
+
+        if (this.variant === 'error') {
+            this.mainCss = this.mainCss.concat('slds-theme_error');
+            this.titleCss += 'slds-text-color_inverse';
+        } else if (this.variant === 'info') {
+            this.mainCss = this.mainCss.concat('slds-theme_info');
+            this.titleCss += 'slds-text-color_inverse';
+        } else { // warning as fallback
+            this.mainCss = this.mainCss.concat('slds-theme_warning');
+        }
+    }
+
+    @wire(getFieldValue, { recordId: '$recordId', field: '$field' })
+    deWire(result) {
+        if (result.data) {
+            this.message = result.data;
+            this.showNotification();
+        }
+    }
+}
