b0203a3b5470bb74a0363892bfed4a3b711cabff
suddal <stine@uddal.com>
2020-09-14 12:59:47 +0200

    Use new department field in component

--- force-app/components/accountTeamMember/lwc/accountTeamMember/accountTeamMember.js
+++ force-app/components/accountTeamMember/lwc/accountTeamMember/accountTeamMember.js
@@ -12,104 +12,106 @@ import Id from '@salesforce/user/Id';
 
 
 const actions = [
-	{ label: 'Slett', name: 'delete' },
+    { label: 'Slett', name: 'delete' },
 ];
 
 const columns = [
-	{ label: 'Navn', fieldName: 'UserId' },
-	{ label: 'Rolle', fieldName: 'TeamMemberRole' },
-	{
-		type: 'action',
-		typeAttributes: { rowActions: actions },
-	},
+    { label: 'Navn', fieldName: 'UserId' },
+    { label: 'Rolle', fieldName: 'TeamMemberRole' },
+    { label: 'Område', fieldName: 'Departments' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    },
 ];
 
 export default class AccountTeamMember extends NavigationMixin(LightningElement) {
-	@api recordId;
-	@track data;
-	@track amount = 0;
-	@track columns = columns;
-	@track showData = false;
+    @api recordId;
+    @track data;
+    @track amount = 0;
+    @track columns = columns;
+    @track showData = false;
 
-	refreshTable;
-	error;
-	userId = Id;
+    refreshTable;
+    error;
+    userId = Id;
 
 
-	@wire(getData, { recordId: '$recordId' })
-	member(result) {
-		this.refreshTable = result;
-		if (result.data) {
-			this.data = result.data;
-			this.amount = result.data.length;
-			this.showData = result.data.length > 0;
+    @wire(getData, { recordId: '$recordId' })
+    member(result) {
+        this.refreshTable = result;
+        if (result.data) {
+            this.data = result.data;
+            this.amount = result.data.length;
+            this.showData = result.data.length > 0;
 
-			let dataList = [];
-			this.data.forEach(element => {
-				let dataElement = {};
-				dataElement.Id = element.Id;
-				dataElement.UserId = element.User.Name;
-				dataElement.TeamMemberRole = element.TeamMemberRole;
-				dataList.push(dataElement);
-			});
+            let dataList = [];
+            this.data.forEach(element => {
+                let dataElement = {};
+                dataElement.Id = element.Id;
+                dataElement.UserId = element.User.Name;
+                dataElement.TeamMemberRole = element.TeamMemberRole;
+                dataElement.Departments = element.Departments__c;
+                dataList.push(dataElement);
+            });
 
-			this.data = dataList;
-		}
-	}
+            this.data = dataList;
+        }
+    }
 
-	handleRowActions(event) {
-		let actionName = event.detail.action.name;
-		let row = event.detail.row;
+    handleRowActions(event) {
+        let actionName = event.detail.action.name;
+        let row = event.detail.row;
 
-		switch (actionName) {
-			case 'delete':
-				this.deleteRow(row);
-				break;
-			default:
-				break;
-		}
-	}
+        switch (actionName) {
+            case 'delete':
+                this.deleteRow(row);
+                break;
+            default:
+                break;
+        }
+    }
 
-	deleteRow(currentRow) {
-		deleteTeamMember({ atmId: currentRow.Id })
-			.then(result => {
-				this.dispatchEvent(new ShowToastEvent({
-					message: 'Kontaktperson ' + currentRow.UserId + ' slettet ',
-					variant: 'success'
-				}));
+    deleteRow(currentRow) {
+        deleteTeamMember({ atmId: currentRow.Id })
+            .then(result => {
+                this.dispatchEvent(new ShowToastEvent({
+                    message: 'Kontaktperson ' + currentRow.UserId + ' slettet ',
+                    variant: 'success'
+                }));
 
-				return refreshApex(this.refreshTable);
+                return refreshApex(this.refreshTable);
 
-			})
-			.catch(error => {
-				this.dispatchEvent(new ShowToastEvent({
-					title: 'Feil',
-					message: error.message,
-					variant: 'error'
-				}));
-			});
-	}
+            })
+            .catch(error => {
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Feil',
+                    message: error.message,
+                    variant: 'error'
+                }));
+            });
+    }
 
-	navigateToNewRecordPage() {
+    navigateToNewRecordPage() {
 
-		const defaultValues = encodeDefaultFieldValues({
-			AccountId: this.recordId,
-			UserId: this.userId
-		});
+        const defaultValues = encodeDefaultFieldValues({
+            AccountId: this.recordId,
+            UserId: this.userId
+        });
 
-		this[NavigationMixin.Navigate]({
-			type: 'standard__objectPage',
-			attributes: {
-				objectApiName: 'AccountTeamMember',
-				actionName: 'new'
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'AccountTeamMember',
+                actionName: 'new'
 
-			}, state: {
-				navigationLocation: 'RELATED_LIST',
-				defaultFieldValues: defaultValues
-			}
-		});
-	}
-	refreshData() {
-		return refreshApex(this.refreshTable);
-	}
+            }, state: {
+                navigationLocation: 'RELATED_LIST',
+                defaultFieldValues: defaultValues
+            }
+        });
+    }
+    refreshData() {
+        return refreshApex(this.refreshTable);
+    }
 }
