ca02132658d2d46b8b87a5ee3f5f8846ad315d7b
piszosta <42918948+piszosta@users.noreply.github.com>
2020-12-04 14:23:48 +0100

    HandleUserMobileInKontaktpersonerListe

--- force-app/components/accountTeamMember/lwc/accountTeamMember/accountTeamMember.js
+++ force-app/components/accountTeamMember/lwc/accountTeamMember/accountTeamMember.js
@@ -20,6 +20,7 @@ const columns = [
     { label: 'Navn', fieldName: 'UserId', initialWidth: 200 },
     { label: 'Rolle', fieldName: 'TeamMemberRole', initialWidth: 200 },
     { label: 'Område', fieldName: 'Departments' },
+    { label: 'Telefon', fieldName: 'MobilePhone', initialWidth: 150 },
     {
         type: 'action',
         typeAttributes: { rowActions: actions },
@@ -53,6 +54,7 @@ export default class AccountTeamMember extends NavigationMixin(LightningElement)
                 dataElement.UserId = element.User.Name;
                 dataElement.TeamMemberRole = element.TeamMemberRole;
                 dataElement.Departments = element.Departments__c;
+                dataElement.MobilePhone = element.User.MobilePhone;
                 dataList.push(dataElement);
             });
 
