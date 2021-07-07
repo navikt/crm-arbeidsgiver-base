35bb312efcec608c8269e53b0f3940db8d10777f
John Føreland <john.foreland@soprasteria.com>
2021-07-07 12:51:26 +0200

    finished AG_GW integration to kontoregister

--- force-app/components/bankAccountNumberViewer/lwc/bankAccountNumberViewer/bankAccountNumberViewer.js
+++ force-app/components/bankAccountNumberViewer/lwc/bankAccountNumberViewer/bankAccountNumberViewer.js
@@ -5,9 +5,24 @@ export default class BankAccountNumberViewer extends LightningElement {
     @api recordId;
 
     isClosed = false;
+    isLoading = true;
+    style = '';
+    text;
 
     @wire(getBankAccountNumber, { recordId: '$recordId' })
-    accountNumber;
+    deWire(result) {
+        if (result.data) {
+            if (result.data.isSuccess) {
+                this.text = result.data.kontonr;
+            } else {
+                this.text = result.data.feilmelding;
+            }
+            this.isLoading = false;
+        } else if (result.error) {
+            this.text = result.error.body.message;
+            this.isLoading = false;
+        }
+    }
 
     openClose() {
         if (this.isClosed) {
