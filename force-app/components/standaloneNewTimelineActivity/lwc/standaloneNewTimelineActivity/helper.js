82b34680349acfeb51b17f04b819099439c58b73
solsen <silje.olsen@nav.no>
2024-12-05 12:00:02 +0100

    flow component that replicate new-buttons on custom timeline

---
+++ force-app/components/standaloneNewTimelineActivity/lwc/standaloneNewTimelineActivity/helper.js
@@ -0,0 +1,90 @@
+export { getFieldValues };
+
+const getFieldValues = (row, recordId) => {
+    let fields = new Array();
+
+    let relationship = getKeyAndValueIfValid(row.SObjectRelationshipField__c, recordId);
+    if (relationship !== null) {
+        fields.push(relationship);
+    }
+
+    let type = getKeyAndValueIfValid(
+        row.Timeline_Child__r.SObjectTypeField__c,
+        row.Timeline_Child__r.SObjectTypeValue__c
+    );
+    if (type !== null) {
+        fields.push(type);
+    }
+
+    let val1 = getKeyAndValueIfValid(
+        row.Timeline_Child__r.CreateableObject_Field1__c,
+        row.Timeline_Child__r.CreateableObject_Value1__c
+    );
+    if (val1 !== null) {
+        fields.push(val1);
+    }
+
+    let val2 = getKeyAndValueIfValid(
+        row.Timeline_Child__r.CreateableObject_Field2__c,
+        row.Timeline_Child__r.CreateableObject_Value2__c
+    );
+    if (val2 !== null) {
+        fields.push(val2);
+    }
+
+    let val3 = getKeyAndValueIfValid(
+        row.Timeline_Child__r.CreateableObject_Field3__c,
+        row.Timeline_Child__r.CreateableObject_Value3__c
+    );
+    if (val3 !== null) {
+        fields.push(val3);
+    };
+
+    let val4 = getKeyAndValueIfValid(
+        row.Timeline_Child__r.CreateableObject_Field4__c,
+        row.Timeline_Child__r.CreateableObject_Value4__c
+    );
+    if (val4 !== null) {
+        fields.push(val4);
+    };
+
+    let val5 = getKeyAndValueIfValid(
+        row.Timeline_Child__r.CreateableObject_Field5__c,
+        row.Timeline_Child__r.CreateableObject_Value5__c
+    );
+    if (val5 !== null) {
+        fields.push(val5);
+    }
+
+    // combine them all
+    let fieldsCombined = '';
+    for (let i = 0; i < fields.length; i++) {
+        fieldsCombined += fields[i] + ',';
+    }
+
+    var d = new Date();
+    fieldsCombined = fieldsCombined.replace('{today}', d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate());
+    return fieldsCombined.substring(0, fieldsCombined.length - 1);
+};
+
+const getKeyAndValueIfValid = (key, value) => {
+    let keyInvalid = isInvalid(key);
+    let valueInvalid = isInvalid(value);
+    if (!(keyInvalid || valueInvalid)) {
+        return key + '=' + value;
+    } else {
+        return null;
+    }
+};
+
+const isInvalid = (input) => {
+    let checkNull = input === null;
+    let checkUndefined = input === undefined;
+    let checkStringNull = input === 'null';
+    let checkListInvalid = false;
+
+    if (!(checkNull || checkUndefined || checkStringNull)) {
+        checkListInvalid = input.includes(';');
+    }
+    return checkNull || checkUndefined || checkStringNull || checkListInvalid;
+};
---
+++ force-app/components/standaloneNewTimelineActivity/lwc/standaloneNewTimelineActivity/standaloneNewTimelineActivity.js
@@ -0,0 +1,61 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import formFactorPropertyName from '@salesforce/client/formFactor';
+import * as helper from './helper';
+import getTimelineObjects from '@salesforce/apex/Timeline_Controller.getTimelineObjects';
+import newObj from '@salesforce/label/c.Timeline_New';
+export default class GenericLogActivities extends NavigationMixin(LightningElement) {
+    @api configIdProperty;
+    showCreateRecords = true;
+    isGrouped = false;
+    @api recordIdProperty;
+    containsMacros;
+    @track sObjects;
+
+    @api get recordId() {
+        console.log('recordIdProperty: ' + this.recordIdProperty);
+        return this.recordIdProperty;
+    }
+    @api get configId() {
+        console.log('configIdProperty: ' + this.configIdProperty);
+        return this.configIdProperty;
+    }
+
+    @wire(getTimelineObjects, { recordId: '$recordId', configId: '$configId' })
+    deWire(result) {
+        if (result.data) {
+            this.sObjects = result.data;
+
+            for (let i = 0; i < result.data.length; i++) {
+                const elem = result.data[i];
+                if (elem.IsMacro__c) {
+                    this.containsMacros = true;
+                }
+            }
+        } else if (result.error) {
+            console.log('error: ' + JSON.stringify(result.error));
+            //this.error = true;
+        }
+    }
+
+    createRecord(event) {
+        const row = this.sObjects[event.target.dataset.index];
+        const override = this.sObjects[event.target.dataset.index].CreateableObject_NoOverride__c == false ? '0' : '1'; // == false to fallback to true if null
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: row.Timeline_Child__r.SObjectName__c,
+                actionName: 'new'
+            },
+            state: {
+                nooverride: override,
+                recordTypeId: row.Timeline_Child__r.CreateableObject_RecordType__c,
+                navigationLocation: 'RECORD_DETAIL',
+                useRecordTypeCheck: 1,
+                defaultFieldValues: helper.getFieldValues(row, this.recordId),
+                backgroundContext: '/lightning/r/' + this.recordId + '/view'
+            }
+        });
+    }
+}
