7f997dd329bd8a93a4c546434aa1a0beb75e9625
solsen <silje.olsen@nav.no>
2025-06-16 16:14:21 +0200

    Update Badge

--- force-app/main/default/lwc/relatedRecordsCardList/relatedRecordsCardList.js
+++ force-app/main/default/lwc/relatedRecordsCardList/relatedRecordsCardList.js
@@ -2,6 +2,45 @@ import { LightningElement, api } from 'lwc';
 
 export default class RelatedRecordsCardList extends LightningElement {
     @api records;
+
+    get recordListAsTiles() {
+        if (this.records && this.records.length > 0) {
+            try {
+                // Create a new list by mapping over the original records
+                const newRecordsList = this.records.map((record) => {
+                    // console.log('RECORD:', JSON.stringify(record));
+
+                    // Ensure fields array exists and has at least one element
+                    if (record.fields && record.fields.length > 0) {
+                        const fieldsCopy = [...record.fields];
+                        const field = fieldsCopy.shift(); // Remove the first field
+                        // console.log('Removed field:', field);
+
+                        return {
+                            ...record,
+                            title: field.value,
+                            recordUrl: `/lightning/r/${record.id}/view`,
+                            fields: fieldsCopy // Keep the remaining fields intact
+                        };
+                    }
+
+                    // Return the record as-is if fields are empty or undefined
+                    return { ...record, fields: [] };
+                });
+
+                //  console.log('New record list:', JSON.stringify(newRecordsList));
+                return newRecordsList;
+            } catch (error) {
+                this.handleError('Error creating recordListTiles', error);
+                return [];
+            }
+        }
+        return [];
+    }
+    /* HELPERS */
+    handleError(message, error) {
+        console.error(`${message}:`, JSON.stringify(error));
+    }
     test = [
         {
             fields: [
