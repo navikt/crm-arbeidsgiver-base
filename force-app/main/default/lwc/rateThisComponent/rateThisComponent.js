4fae0b2916a1d3257d9d6c4ea0a8fa108c345063
solsen <silje.olsen@nav.no>
2025-08-18 15:42:27 +0200

    Platform event for feedback
    Platform event for feedback
    
    Renamed variables
    
    Endret feltnavn
    
    Update POAGDataHandler.cls
    
    Fjernet delete funksjon

--- force-app/main/default/lwc/rateThisComponent/rateThisComponent.js
+++ force-app/main/default/lwc/rateThisComponent/rateThisComponent.js
@@ -1,7 +1,6 @@
 import { LightningElement, track, api } from 'lwc';
-import hasRecords from '@salesforce/apex/POAGDataHandler.hasRecords';
+import hasVoted from '@salesforce/apex/POAGDataHandler.hasVoted';
 import handleNewRating from '@salesforce/apex/POAGDataHandler.handleNewRating';
-import handleDeleteRating from '@salesforce/apex/POAGDataHandler.handleDeleteRating';
 import userId from '@salesforce/user/Id';
 
 export default class RateThisComponent extends LightningElement {
@@ -10,7 +9,8 @@ export default class RateThisComponent extends LightningElement {
     // =========================
 
     userId = userId; // Use the imported user ID
-    @api voteKey; // Key for the component, can be set from a parent component or page property
+    @api componentName; // Name of the component.
+
     @api question; // Optional question to display when not rated
     @api thankYouMessage; // Optional thank you message to display after rating
     @track _displayThankYouMessage = false;
@@ -47,60 +47,46 @@ export default class RateThisComponent extends LightningElement {
 
         this._hasCheckedDatabase = true;
 
-        hasRecords({ voteKey: this.voteKey, userId: this.userId })
+        hasVoted({ componentName: this.componentName, userId: this.userId })
             .then((data) => {
                 this._isRated = data;
-                console.log(`Database check complete for key ${this.voteKey}:`, data);
+                console.log(`Database check complete for key ${this.componentName}:`, data);
             })
             .catch((error) => {
                 this.handleError('Error retrieving previous rating', error);
-                this._hasCheckedDatabase = false; // Allow retry on error
+                //this._hasCheckedDatabase = false; // Allow retry on error. Causing loop issue
             });
     }
 
     saveRating(rating) {
         // Logic to save the rating to the database
-        handleNewRating({ voteKey: this.voteKey, userId: this.userId, liked: rating })
+        handleNewRating({ componentName: this.componentName, userId: this.userId, vote: rating })
             .then(() => {
                 console.log('Rating saved successfully');
-                this._isRated = true; // Update local state to prevent further voting
                 this._displayThankYouMessage = true; // Show thank you message after rating
             })
             .catch((error) => {
+                this._isRated = false;
                 this.handleError('Error saving rating', error);
             });
     }
 
-    deleteRating() {
-        // Logic to delete the rating from the database
-        handleDeleteRating({ voteKey: this.voteKey, userId: this.userId })
-            .then(() => {
-                console.log('Rating deleted successfully');
-                this._isRated = false; // Update local state to allow voting again
-            })
-            .catch((error) => {
-                this.handleError('Error deleting rating', error);
-            });
-    }
     // =========================
     // EVENT HANDLERS
     // =========================
 
     handleLikeClick() {
         if (!this._isRated) {
-            this.saveRating(true);
+            this._isRated = true;
+            this.saveRating(1);
         }
-    }
-
-    handleUndoClick() {
-        // Logic to undo the rating
-        this.deleteRating();
     }
 
     handleDislikeClick() {
         // Handle dislike button click
         if (!this._isRated) {
-            this.saveRating(false);
+            this._isRated = true;
+            this.saveRating(-1);
         }
     }
 
