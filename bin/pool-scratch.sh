db5f861783b6e87e859ed8749909ddcc3b3e4ac4
Andre Colle <andre.colle@nav.no>
2026-02-13 11:35:15 +0100

    Add lists to candidate outcome

--- bin/pool-scratch.sh
+++ bin/pool-scratch.sh
@@ -115,6 +115,7 @@ assignPermission() {
     --name CRM_LoginFlow \
     --name TAG_Arbeidsgiver_Veillederapp \
     --name Arbeidsgiver_Beta_app \
+    --name Arbeidsgiver_Kandidatutfall \
     || { error $? '"sf org assign permset" command failed.'; }
 }
 
