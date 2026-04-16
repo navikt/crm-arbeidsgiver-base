7ec376eaaa121f980c0bcd73109e04b732eb65e8
piszosta <piotr.szostak@nav.no>
2026-04-16 16:05:21 +0200

    next best action card component with embedded screenflow functionality

---
+++ force-app/components/nextBestActionCard/lwc/nextBestActionCard/nextBestActionCard.js
@@ -0,0 +1,39 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class NextBestActionCard extends LightningElement {
+
+    @api recordId;
+    @api cardTitle   = 'Title';
+    @api cardText    = 'Add a description for next best action.';
+    @api buttonLabel = 'Create record';
+    @api flowApiName;
+
+    @track showFlowModal = false;
+
+    handleAction() {
+        event.stopPropagation();
+        if (!this.flowApiName) {
+            console.warn('nextBestActionCard: no flowApiName configured.');
+            return;
+        }
+        this.showFlowModal = true;
+        requestAnimationFrame(() => {
+            const flowCmp = this.template.querySelector('lightning-flow');
+            if (flowCmp) {
+                flowCmp.startFlow(this.flowApiName, [
+                    { name: 'recordId', type: 'String', value: this.recordId }
+                ]);
+            }
+        });
+    }
+
+    handleStatusChange(event) {
+        if (event.detail.status === 'FINISHED' || event.detail.status === 'FINISHED_SCREEN') {
+            this.closeModal();
+        }
+    }
+
+    closeModal() {
+        this.showFlowModal = false;
+    }
+}
