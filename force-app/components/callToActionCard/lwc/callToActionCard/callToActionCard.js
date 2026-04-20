d9387eb3f615a757e3c8015266cfbb67cc891463
piszosta <piotr.szostak@nav.no>
2026-04-20 14:20:24 +0200

    component files callToActionCard

--- force-app/components/callToActionCard/lwc/callToActionCard/callToActionCard.js
+++ force-app/components/callToActionCard/lwc/callToActionCard/callToActionCard.js
@@ -1,6 +1,6 @@
 import { LightningElement, api, track } from 'lwc';
 
-export default class NextBestActionCard extends LightningElement {
+export default class CallToActionCard extends LightningElement {
 
     @api recordId;
     @api cardTitle   = 'Title';
